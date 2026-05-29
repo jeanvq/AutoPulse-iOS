ea6971049ae5735ce864c2126340a04cb0fad5ce
Jeancarlo <jeancarlo.ricardo@triosstudent.com>
2026-05-29 18:20:17 -0400

    Initial Commit

---
+++ AutoPulse/AutoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPulseApp.swift
+//  AutoPulse
+//
+//  Created by Jeancarlo on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct AutoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoPulseTests/AutoPulseTests.swift
@@ -0,0 +1,19 @@
+//
+//  AutoPulseTests.swift
+//  AutoPulseTests
+//
+//  Created by Jeancarlo on 2026-05-29.
+//
+
+import Testing
+@testable import AutoPulse
+
+struct AutoPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
