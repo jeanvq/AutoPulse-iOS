ede99b76f03aa13344315cfcaebda9ef77e371ec
Jeancarlo <jeancarlo.ricardo@triosstudent.com>
2026-09-08 18:30:48 -0400

    Add Maintenance records with Firestore

---
+++ AutoPulse/ViewModels/MaintenanceViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MaintenanceViewModel.swift
+//  AutoPulse
+//
+//  Created by Jeancarlo on 2026-09-08.
+//
+
+import Foundation
---
+++ AutoPulse/Views/Maintenance/AddMaintenanceRecordView.swift
@@ -0,0 +1,8 @@
+//
+//  AddMaintenanceRecordView.swift
+//  AutoPulse
+//
+//  Created by Jeancarlo on 2026-09-08.
+//
+
+import Foundation
